3327049e841e142d0dbf7297c51fc0e28ff09ff3
rulin <rulin8@gmail.com>
2011-09-24 20:46:26 -0400

    Replaced html5 player (from audio.js to jplayer) Right sort dir and files

--- www/index.php
+++ www/index.php
@@ -10,7 +10,6 @@ if (preg_match('/^(.*)(?:$|\?)/U',$_SERVER['REQUEST_URI'], $match)) {
 	$path = preg_replace('/\/\.\.(\/|$)/','/',$match['1']);
 }
 
-
 $currentAudioPath = trim($path);//trim(AUDIO_PATH.$path);
 // Удаляем слеш на конце
 if (strlen($currentAudioPath) > 0 && $currentAudioPath[strlen($currentAudioPath)-1] == '/') {
@@ -26,15 +25,20 @@ if (DIRECTORY_SEPARATOR == '/') {
 	$currentAudioPath = str_replace('/','\\',$currentAudioPath);	
 }
 
-$html_body = '';
+$htmlBack = '';
+$htmlFile = '';
+$htmlDir = '';
 
 if ($currentAudioPath != '') {
-	$html_body .= '<div class="dir"><p><a href="../"><< Назад</a></p></div>';	
+	$htmlBack = '<div class="dir"><p><a href="../"><< Назад</a></p></div>';	
 }
 
 $currentAudioPathWeb = AUDIO_PATH_WEB.$currentAudioPath;
 $currentAudioPath = AUDIO_PATH.$currentAudioPath;
 
+$dirArray = array();
+$fileArray = array();
+$musicArray = array();
 
 if ($currentAudioPath !== false) {
 	if (is_dir($currentAudioPath)) {
@@ -42,19 +46,54 @@ if ($currentAudioPath !== false) {
 			while (($file = readdir($dh)) !== false) {
 				if ($file != '.' && $file != '..') {					
 					if (is_dir($currentAudioPath.DIRECTORY_SEPARATOR.$file)) {
-						$html_body .= '<div class="dir"><a href="'.$file.'/">'.$file.'</a></div>';
+						$dirArray[$file] = array(
+							'title' => $file,
+							'path' => $file.'/',
+						);	 						 						
 					} else {
-						if (preg_match('/(\.mp3|\.ogg|\.wav)$/i',$file)) 
-							$html_body .= '<div class="file"><p>Послушать:<audio src="'.$currentAudioPathWeb.'/'.$file.'" preload="auto" /></p><p>Скачать: <a href="'.$currentAudioPathWeb.'/'.$file.'">'.$file.'</a></p></div>';
+						if (preg_match('/(\.mp3)$/i',$file)) {
+							$pathinfo = pathinfo($file);							
+							$musicArray[$pathinfo['filename']] = array(
+								'title' => $pathinfo['filename'],
+								$pathinfo['extension'] => $currentAudioPathWeb.'/'.$file,
+							);
+						}
+						$fileArray[$file] = array(
+							'title' => $file,
+							'path' => $currentAudioPathWeb.'/'.$file,
+						);	
 					}
-					
-				}								
-				
+				}	
 			}
 			closedir($dh);
 		}
 	}
 }
+
+rsort($dirArray);
+sort($musicArray);
+sort($fileArray);
+
+foreach ($dirArray as $value) 
+	$htmlDir .= '<div class="dir"><a href="'.$value['path'].'">'.$value['title'].'</a></div>';
+
+foreach ($fileArray as $value)
+	$htmlFile .= '<div class="file"><a href="'.$value['path'].'">'.$value['title'].'</a></div>';
+
+$jPlayerCssSelector = array(
+	'jPlayer' => "#jquery_jplayer_1",
+	'cssSelectorAncestor' => "#jp_container_1"
+	);
+$jPlayerPlaylist = $musicArray;
+$jPlayerOptions = array(	
+	'swfPath' => '/js/jQuery.jPlayer.2.1.0',
+	'supplied' => 'mp3',
+	'wmode' =>  'window',
+);
+
+$jPlayerCssSelector = json_encode($jPlayerCssSelector);
+$jPlayerPlaylist = json_encode($jPlayerPlaylist);
+$jPlayerOptions = json_encode($jPlayerOptions);
 
 require TEMPLATE_PATH.'/index.tpl.inc';
 
