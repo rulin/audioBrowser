919fd4ae30dc38174b5a2cdf3540f679f456be3a
rulin <rulin8@gmail.com>
2011-09-24 21:04:41 -0400

    remove old player files

--- www/config.php
+++ www/config.php
@@ -4,4 +4,3 @@ define('DOCUMENT_ROOT', dirname(__FILE__));
 define('TEMPLATE_PATH', DOCUMENT_ROOT.DIRECTORY_SEPARATOR.'template');
 define('AUDIO_PATH', DOCUMENT_ROOT.DIRECTORY_SEPARATOR.'audio');
 define('AUDIO_PATH_WEB', '/audio');
-
